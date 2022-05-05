0d0b735c908685206251cc500b33659a88c05c0a
Charleston Amaral <charleston.silva03@gmail.com>
2022-05-05 17:21:50 -0300

    refactor: add new props and custom component card project

--- src/components/molecules/CardProject/index.tsx
+++ src/components/molecules/CardProject/index.tsx
@@ -1,17 +1,44 @@
+import Image from "next/image"
+import Link from "next/link"
+
 import { Paragraph } from "../../atoms/Paragraph"
 import { Subtitle } from "../../atoms/Subtitle"
+
+import ArrowRightIcon from '../../../../public/assets/arrowRight.svg'
 
 export interface CardProjectProps {
   title: string
   description: string
+  url: string
 }
 
-function CardProject({ description, title }: CardProjectProps) {
+function CardProject({ description, title, url }: CardProjectProps) {
   return (
-    <article className="flex flex-col gap-2">
-      <Subtitle>{title}</Subtitle>
-      <Paragraph>{description}</Paragraph>
-    </article>
+    <div className="flex flex-col gap-4 items-end">
+      <article className="flex flex-col flex-1 gap-2">
+        <Subtitle>{title}</Subtitle>
+        <Paragraph>{description}</Paragraph>
+      </article>
+
+      <Link href={url}>
+        <a
+          className="flex justify-center gap-4 text-newGray-700 font-medium opacity-80 hover:opacity-100 transition-opacity"
+          rel="noopener noreferrer" 
+          target="_blank" 
+          title={title}
+        >
+          Ver Projeto
+          
+          <Image
+            src={ArrowRightIcon}
+            width={24}
+            height={24}
+            alt="Seta indicativa para o lado direito"
+            className="w-6 h-6"
+          />
+        </a>
+      </Link>
+    </div>
   )
 }
 
