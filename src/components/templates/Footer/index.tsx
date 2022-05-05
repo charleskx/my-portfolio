345ad87fa7dafd2b7ac89033e87ec45d58592fc0
Charleston Amaral <charleston.silva03@gmail.com>
2022-05-05 16:10:40 -0300

    refactor: include component paragraph in footer

--- src/components/templates/Footer/index.tsx
+++ src/components/templates/Footer/index.tsx
@@ -1,13 +1,14 @@
 import { Logo } from "../../atoms/Logo"
+import { Paragraph } from "../../atoms/Paragraph"
 
 function Footer() {
   return (
     <footer className="flex gap-1 flex-col px-6 py-8 bg-white">
       <Logo>Charleston Amaral.</Logo>
 
-      <p className="text-xs leading-4 text-newGray-700">
+      <Paragraph>
         Apaixonado por programação e empreendedorismo. Formado em Análise e Desenvolvimento de Sistemas, com mais de 10 anos de experiência na localização e implementação de soluções tecnológicas e gerenciamento de equipes.
-      </p>
+      </Paragraph>
     </footer>
   )
 }
