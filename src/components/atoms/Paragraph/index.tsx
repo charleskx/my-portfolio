067bcdc6c5616aa542ddca73fb48cff589da5a1c
Charleston Amaral <charleston.silva03@gmail.com>
2022-05-05 16:39:19 -0300

    refactor: add classname props in paragraph component

--- src/components/atoms/Paragraph/index.tsx
+++ src/components/atoms/Paragraph/index.tsx
@@ -2,11 +2,15 @@ import { HTMLAttributes, ReactNode } from "react";
 
 export interface ParagraphProps extends HTMLAttributes<HTMLParagraphElement> {
   children: ReactNode
+  className: string
 }
 
-function Paragraph({ children, ...props }: ParagraphProps) {
+function Paragraph({ children, className, ...props }: ParagraphProps) {
   return (
-    <p className="text-newGray-700 text-xs leading-4" {...props}>
+    <p
+      className={`text-newGray-700 text-xs leading-4 ${className}`}
+      {...props}
+    >
       {children}
     </p>
   )
