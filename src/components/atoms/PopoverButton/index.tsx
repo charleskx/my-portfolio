54cd872346518c7e2d4e0718a1ea8d074ec23a0e
Charleston Amaral <charleston.silva03@gmail.com>
2022-05-11 15:21:51 -0300

    fix: accessible name buttons

--- src/components/atoms/PopoverButton/index.tsx
+++ src/components/atoms/PopoverButton/index.tsx
@@ -8,11 +8,11 @@ export interface PopoverButtonProps {
 
 function PopoverButton({ children, alt }: PopoverButtonProps) {
   return (
-    <Popover.Button className="w-6 h-6 flex justify-center items-center m-0 p-0 opacity-80 hover:opacity-100 transition-opacity">
-      <>
-        <span className="hidden">{alt}</span>
-        {children}
-      </>
+    <Popover.Button
+      className="w-6 h-6 flex justify-center items-center m-0 p-0 opacity-80 hover:opacity-100 transition-opacity"
+      aria-label={alt}
+    >
+      {children}
     </Popover.Button>
   )
 }
