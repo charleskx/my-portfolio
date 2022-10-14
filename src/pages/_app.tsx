c1a398b3c11692d1e1f293e5d773a4012371fb91
Charleston Amaral <charleston.silva03@gmail.com>
2022-10-13 22:26:01 -0300

    feat: add and configure lib hotjar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
+import { useEffect } from 'react'
 import { ThemeProvider } from 'next-themes'
+import { hotjar } from 'react-hotjar'
 import type { AppProps } from 'next/app'
 
 import '../../styles/globals.css'
+
+useEffect(() => {
+  hotjar.initialize(3201606, 6)
+}, [])
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
