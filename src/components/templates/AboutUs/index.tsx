26f5c17e5db926bfeca1c91511734b5d76dd8bfb
Charleston Amaral <charleston.silva03@gmail.com>
2023-01-24 18:37:58 -0300

    feat: add new jobs

--- src/components/templates/AboutUs/index.tsx
+++ src/components/templates/AboutUs/index.tsx
@@ -3,8 +3,8 @@ import { Experiences } from "../../organisms/Experiences"
 const experiences = [
   {
     id: 1,
-    title: 'Analista de Sistemas - Unidata',
-    description: 'Soluções de automação para controle de abastecimento. UI e desenvolvedor front-end utilizando ReactJS para criação de dashboards e soluções personalizadas.'
+    title: 'Programador Pleno - Mundo Wap',
+    description: 'Solução focada na gestão de operações de Merchandising e Trade Marketing. Web full-stack utilizando PHP para gerar relatórios de clientes, produtos e métricas de vendas. Front-end utilizando ReactJS para desenvolvimento de soluções.'
   },
   {
     id: 2,
@@ -13,15 +13,20 @@ const experiences = [
   },
   {
     id: 3,
-    title: 'Programador Pleno - Mundo Wap',
-    description: 'Solução focada na gestão de operações de Merchandising e Trade Marketing. Web full-stack utilizando PHP para gerar relatórios de clientes, produtos e métricas de vendas. Front-end utilizando ReactJS para desenvolvimento de soluções.'
+    title: 'Analista de Sistemas - Unidata',
+    description: 'Soluções de automação para controle de abastecimento. UI e desenvolvedor front-end utilizando ReactJS para criação de dashboards e soluções personalizadas.'
+  },
+  {
+    id: 4,
+    title: 'Analista de Desenvolvimento de Sistemas - Instituto Brasileiro de Tecnologia e Ciência da Computação IBTCC',
+    description: 'Desenvolvimento de APIs utilizando AWS Lambda e NodeJS além da criação de dashboards com ReactJS e Typescript.'
   }
 ]
 
 function AboutUs() {
   return (
     <section id="aboutUs" className="px-6 pt-9 pb-16 bg-newGray-50 dark:bg-newGray-800 md:px-32 md:py-24 md:flex md:flex-col md:items-center">
-      <Experiences experiences={experiences} />
+      <Experiences experiences={experiences.sort((a, b) => b.id - a.id)} />
     </section>
   )
 }
